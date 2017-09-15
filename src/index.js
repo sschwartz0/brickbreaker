2b0d94c4cf873f883e4950a7885a3de9ed6512fa
sschwartz <scottmschwrtz@gmail.com>
2017-09-14 23:52:16 -0700

    added thunk. actions working

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { AppContainer } from 'react-hot-loader';
 import reducer from './reducers'
 import App from './modules/App'
 
 let store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  reducer, compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  )
 );
 
 ReactDOM.render(
